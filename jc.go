d062b1ce5d90fb4f6dd6e791f8e834874bd9f595
taskie <t@skie.jp>
2019-01-29 09:22:17 +0900

    separate to Encoder and Decoder

--- jc.go
+++ jc.go
@@ -3,23 +3,20 @@ package jc
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/BurntSushi/toml"
 	"github.com/vmihailenco/msgpack"
 	"gopkg.in/yaml.v2"
-	"io"
-	"strings"
 )
 
 var (
 	Version  = "0.1.0-beta"
 	Revision = ""
 )
-
-type Jc struct {
-	FromType string
-	ToType   string
-	Indent   *string
-}
 
 func cleanDataFromYaml(data interface{}) interface{} {
 	switch oldData := data.(type) {
@@ -43,17 +40,29 @@ func cleanDataFromYaml(data interface{}) interface{} {
 	}
 }
 
-func (jc *Jc) Decode(r io.Reader, data interface{}) error {
-	switch strings.ToLower(jc.FromType) {
+type Decoder struct {
+	Reader io.Reader
+	Type   string
+}
+
+func NewDecoder(r io.Reader, ty string) *Decoder {
+	return &Decoder{
+		Reader: r,
+		Type:   ty,
+	}
+}
+
+func (jcDec *Decoder) Decode(data interface{}) error {
+	switch strings.ToLower(jcDec.Type) {
 	case "json":
-		dec := json.NewDecoder(r)
+		dec := json.NewDecoder(jcDec.Reader)
 		err := dec.Decode(data)
 		return err
 	case "toml":
-		_, err := toml.DecodeReader(r, data)
+		_, err := toml.DecodeReader(jcDec.Reader, data)
 		return err
 	case "yaml":
-		dec := yaml.NewDecoder(r)
+		dec := yaml.NewDecoder(jcDec.Reader)
 		err := dec.Decode(data)
 		if err != nil {
 			return err
@@ -63,42 +72,111 @@ func (jc *Jc) Decode(r io.Reader, data interface{}) error {
 		}
 		return nil
 	case "msgpack":
-		dec := msgpack.NewDecoder(r)
+		dec := msgpack.NewDecoder(jcDec.Reader)
 		err := dec.Decode(data)
 		return err
 	default:
-		return fmt.Errorf("invalid --from type: %s", jc.FromType)
+		return fmt.Errorf("invalid --from type: %s", jcDec.Type)
 	}
 }
 
-func (jc *Jc) Encode(w io.Writer, data interface{}) error {
-	switch strings.ToLower(jc.ToType) {
+func DecodeFile(fpath string, ty string, data interface{}) error {
+	f, err := os.Open(fpath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if ty == "" {
+		ty = ExtToTypeMap(filepath.Ext(fpath))
+	}
+	return NewDecoder(f, ty).Decode(data)
+}
+
+type Encoder struct {
+	Writer io.Writer
+	Type   string
+	Indent *string
+}
+
+func NewEncoder(w io.Writer, ty string) *Encoder {
+	return &Encoder{
+		Writer: w,
+		Type:   ty,
+	}
+}
+
+func (jcEnc *Encoder) Encode(data interface{}) error {
+	switch strings.ToLower(jcEnc.Type) {
 	case "json":
-		enc := json.NewEncoder(w)
+		enc := json.NewEncoder(jcEnc.Writer)
 		enc.SetEscapeHTML(false)
-		if jc.Indent != nil {
-			enc.SetIndent("", *jc.Indent)
+		if jcEnc.Indent != nil {
+			enc.SetIndent("", *jcEnc.Indent)
 		}
 		err := enc.Encode(data)
 		return err
 	case "toml":
-		enc := toml.NewEncoder(w)
-		if jc.Indent != nil {
-			enc.Indent = *jc.Indent
+		enc := toml.NewEncoder(jcEnc.Writer)
+		if jcEnc.Indent != nil {
+			enc.Indent = *jcEnc.Indent
 		}
 		err := enc.Encode(data)
 		return err
 	case "yaml":
-		enc := yaml.NewEncoder(w)
+		enc := yaml.NewEncoder(jcEnc.Writer)
 		err := enc.Encode(data)
 		return err
 	case "msgpack":
-		enc := msgpack.NewEncoder(w)
+		enc := msgpack.NewEncoder(jcEnc.Writer)
 		err := enc.Encode(data)
 		return err
 	default:
-		return fmt.Errorf("invalid --to type: %s", jc.ToType)
+		return fmt.Errorf("invalid --to type: %s", jcEnc.Type)
 	}
+}
+
+func EncodeFile(fpath string, ty string, data interface{}) error {
+	f, err := os.Create(fpath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if ty == "" {
+		ty = ExtToTypeMap(filepath.Ext(fpath))
+	}
+	return NewEncoder(f, ty).Encode(data)
+}
+
+var extToTypeMap = map[string]string{
+	".json":        "json",
+	".toml":        "toml",
+	".yml":         "yaml",
+	".yaml":        "yaml",
+	".mp":          "msgpack",
+	".msgpack":     "msgpack",
+	".messagepack": "msgpack",
+}
+
+func ExtToTypeMap(ext string) string {
+	ty := extToTypeMap[ext]
+	if ty != "" {
+		return ty
+	}
+	return "json"
+}
+
+type Jc struct {
+	FromType string
+	ToType   string
+	Indent   *string
+}
+
+func (jc *Jc) Decode(r io.Reader, data interface{}) error {
+	return NewDecoder(r, jc.FromType).Decode(data)
+}
+
+func (jc *Jc) Encode(w io.Writer, data interface{}) error {
+	return NewEncoder(w, jc.ToType).Encode(data)
 }
 
 func (jc *Jc) Run(r io.Reader, w io.Writer) error {
